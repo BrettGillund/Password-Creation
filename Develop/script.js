cb4e9b06a454b5d79c82b438354f5d1d5a3fd03c
Brett Gillund <brettgillund2@gmail.com>
2022-06-27 22:29:41 -0500

    attempting to reformat JS to combine arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,96 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
+// var count = 0;
 
 function generatePassword() {
-  var uppercase = confirm('uppercase'); // true or false
-  var lowercase = confirm('lowercase'); // true or false
-  var passwordLength = prompt('chose password length between 8 and 128');
-  var passwordResult = '';
+    var wantsLength = prompt('Please choose a length between 8 and 128.');
+        wantsLength = Number(wantsLength); // This converts the string provided by the prompt into a Number type. 
+    var wantsUppercase = confirm('Please press "Okay" if you would you like to include uppercase letter letter(s) in your password?');
+    var wantsLowercase = confirm('Please press "Okay" if you would you like to include lowercase letter letter(s) in your password?');
+    var wantsSpecialChar = confirm('Please press "Okay" if you would you like to include special character(s) in your password?');
+    var wantsNumbers = confirm('Please press "Okay" if you would you like to include number(s) in your password?');
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var specialChar = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~';
+    var numbers = '0123456789'
+    var passwordResult = '';
+    var passwordE1 = document.querySelector('#password');
 
-  length =Number(length);
 
-  if ( uppercase ) {
-    // add some uppercase letters to our result
-  } else if (lowercase === true) {
-    // add some lowercase letters to our result
-  } else if (passLength >= 8, passLength <= 128)
 
-  return '';
+
+
+console.log(charactersIncluded)
+
+    for (var count = 0; count < wantsLength ; count++)  {
+        passwordResult += charactersIncluded[Math.floor(Math.random() * (charactersIncluded.length - 1) + 1)];
+
+    }
+
+
+    passwordE1.innerText = passwordResult;
 }
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+generateBtn.addEventListener('click', generatePassword)
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+// TO BE DELTEED
+// line 6 (wantsLength = Number(wantsLength) how do we change a string in JS into a number??? We will need to do this to get the number of characters I am assuming.
+
+// Line 16 over for statement    // he replaced length with something becauase he said it would be too obvious. try using wantsLength = Number(wantsLength) then maybe wantsLength in the equasion.
+
+    // console.log (wantsLength, wantsUppercase, wantsLowercase, wantsSpecialChar, specialChar)
+
+     // console.log(specialChar[specialChar.length - 1])
+
+    //  function charactersIncluded (A B C D) {
+    //     if (wantsUppercase == true) {
+            
+    //     }
+    //     if (wantsLowercase == true){
+
+    //     }
+    //     if (wantsSpecialChar == true){
+
+    //     }
+    //     if (wantsNumbers == true) {
+
+    //     }
+
+    //     characters = [uppercase, lowercase, specialChar, numbers]
+
+    //     return characters
+    // }
+
+        // const charactersIncluded = {
+    //     ...(uppercase && {uppercase}),
+    //     ...(lowercase && {lowercase}),
+    //     ...(specialChar && {specialChar}),
+    //     ...(numbers && {numbers}),
+    // }
+
+
+
+//   var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+//   var specialChar = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~';
+//   var numbers = '0123456789'
+//   var passwordResult = '';
+//   var passwordE1 = document.querySelector('#password');
+
+
+// function whatCharacters (U, L, S, N) {
+//   console.log(U, L, S, N)
+
+// }
+
+// function areWeUpper (){
+//   if (wantsUppercase) {
+//     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//   }
+//   return uppercase
+// }
+// console.log(areWeUpper)
+// }
